236244ed22ee812949ccb7a784e43bc2d3160d41
creative-og <creative | Alex>
2022-09-17 02:08:55 -0700

    add colors

--- rectFun.js
+++ rectFun.js
@@ -7,6 +7,7 @@ export default class rectangle{
         this.dx = 10
         this.dy = 10
         this.keyCode = 0
+        this.color = ''
     }
 
     draw(){
@@ -19,20 +20,23 @@ export default class rectangle{
         this.draw()
         if(this.keyCode == 37)
         {
+            this.color = '#151F30'
             this.x -= Math.abs(this.dy)
-            if(this.x+90 < 0){
+            if(this.x < 0){
                 this.x = innerWidth
             }
         }
         if(this.keyCode == 38)
         {
+            this.color = '#103778'
             this.y -= Math.abs(this.dy)
-            if(this.y+60 < 0){
+            if(this.y < 0){
                 this.y = innerHeight
             }
         }
         if(this.keyCode == 39)
         {
+            this.color = '#0593A2'
             this.x += Math.abs(this.dx)
             if(this.x > innerWidth){
                 this.x = 0
@@ -40,6 +44,7 @@ export default class rectangle{
         }
         if(this.keyCode == 40)
         {
+            this.color = '#E3371E'
             this.y += Math.abs(this.dy)
             if(this.y > innerHeight){
                 this.y = 0
